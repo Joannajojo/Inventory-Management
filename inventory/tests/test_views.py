0dc93c8b9021def43f80938c13935dddc1472262
Jo_anna <annajo7512@gmail.com>
2022-01-27 11:00:38 +0800

    fixed test cases

--- inventory/tests/test_views.py
+++ inventory/tests/test_views.py
@@ -1,23 +1,25 @@
 from django.urls import reverse
 from django.test import TestCase
 from django.test import Client
+from inventory.models import Inventory,Supplier
 
 
 class InventoryTestClass(TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
-        
+        self.supplier=Supplier.objects.create(name='Aikeyar')
+        Inventory.objects.create(name='Fantasia', description = 'Dining table', note = '', stock = 5,availability = True, supplier = self.supplier)
 
     def test_inventory_list_view(self):
         response = self.client.get('/inventory/')
         self.assertEqual(response.status_code, 200)
-        #self.assertEqual(1 + 1, 3)
+        
 
     def test_inventory_detail_view(self):
-        response = self.client.get('/inventory/2/')
+        response = self.client.get('/inventory/1')
         self.assertEqual(response.status_code, 200)
 
-    #def test_inventory_api_view(self):
-     #   response = self.client.get('/inventory/api/inventory')
-      #  self.assertEqual(response.status_code, 200)
+    def test_inventory_api_view(self):
+        response = self.client.get('/inventory/api/inventory')
+        self.assertEqual(response.status_code, 200)
