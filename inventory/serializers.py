a5fe3f51df1bf1e40efd3b39c8a57b63ae715f99
Jo_anna <annajo7512@gmail.com>
2022-01-27 10:20:41 +0800

    updates

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -4,12 +4,6 @@ from .models import Supplier,Inventory
 from rest_framework import serializers
 
 class InventorySerializer(serializers.ModelSerializer):
-    #name= serializers.CharField(max_length=80)
-    #description = serializers.CharField(max_length=150)
-    #note = serializers.CharField(max_length=150)
-    #stock = serializers.IntegerField()
-    #availability = serializers.BooleanField()
-    #supplier = serializers.ForeignKey(Supplier, on_delete=serializers.CASCADE)
     
     class Meta:
         model = Inventory
--- inventory/tests/test_views.py
+++ inventory/tests/test_views.py
@@ -1,8 +1,23 @@
+from django.urls import reverse
+from django.test import TestCase
+from django.test import Client
 
-def test_view(self):
-        w = self.create_whatever()
-        url = reverse("views.whatever")
-        resp = self.client.get(url)
 
-        self.assertEqual(resp.status_code, 200)
-        self.assertIn(w.title, resp.content)
+class InventoryTestClass(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+        
+
+    def test_inventory_list_view(self):
+        response = self.client.get('/inventory/')
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(1 + 1, 3)
+
+    def test_inventory_detail_view(self):
+        response = self.client.get('/inventory/2/')
+        self.assertEqual(response.status_code, 200)
+
+    #def test_inventory_api_view(self):
+     #   response = self.client.get('/inventory/api/inventory')
+      #  self.assertEqual(response.status_code, 200)
